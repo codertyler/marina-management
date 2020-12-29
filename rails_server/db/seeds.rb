5a81c2a979f5404accc49dd63cead13b6bab7e3f
codertylerasai@gmail.com <codertylerasai@gmail.com>
2020-12-28 18:19:45 -0700

    installed rails server and react app

---
+++ rails_server/db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+boats = Boat.create([
+  {
+    name: "Peppa",
+    length: 10.5,
+    color: "green"
+
+  },
+  {
+    name: "George",
+    length: 7.5,
+    color: "blue",
+  },
+  {
+    name: "Ms Rabbit",
+    length: 8,
+    color: "red"
+  }
+])
